d9d9a91079a3c4791df34d2306edf217c96b4f21
dmeyer4569 <dmeyer2@oakland.edu>
2026-03-14 18:34:34 -0400

    can create a new case

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,12 @@ import asyncio
 from contextlib import asynccontextmanager
 from models.database import db_manager
 from models.writer import insert_case, insert_simulation, insert_round, insert_persona
-from models.reader import read_cases
+from models.reader import *
+from models.writer import * 
 
 from fastapi import FastAPI
 from routes import read_router
+from routes import write_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,7 +18,7 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 
 app.include_router(read_router.router)
-
+app.include_router(write_router.router)
 
 ### Put fastapi stuff here, ensure use of routers to decrease random stuff in her### Put fastapi stuff here, ensure use of routers to decrease random stuff in heree
 @app.get("/")
